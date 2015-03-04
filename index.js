e921a480d57c0b1793ee9cd33db07127182ebae1
shivkanth <shivkanthb@gmail.com>
2015-03-04 01:35:47 -0700

    fb integration

--- index.js
+++ index.js
@@ -4,6 +4,112 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+
+ var passport          =     require('passport')
+  , util              =     require('util')
+  , FacebookStrategy  =     require('passport-facebook').Strategy
+  , session           =     require('express-session')
+  , cookieParser      =     require('cookie-parser')
+  , bodyParser        =     require('body-parser')
+  , config            =     require('./configuration/config')
+  , mysql             =     require('mysql');
+
+//Define MySQL parameter in Config.js file.
+var connection = mysql.createConnection({
+  host     : config.host,
+  user     : config.username,
+  password : config.password,
+  database : config.database
+});
+
+//Connect to Database only if Config.js parameter is set.
+
+if(config.use_database==='true')
+{
+    connection.connect();
+}
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      if(config.use_database==='true')
+      {
+      connection.query("SELECT * from user_info where user_id="+profile.id,function(err,rows,fields){
+        if(err) throw err;
+        if(rows.length===0)
+          {
+            console.log("There is no such user, adding now");
+            connection.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+          }
+          else
+            {
+              console.log("User already exists in database");
+            }
+          });
+      }
+      return done(null, profile);
+    });
+  }
+));
+
+
+//app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/index1', function(req, res){
+  res.render('index1', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/index1', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
@@ -43,6 +149,7 @@ io.on('connection', function (socket) {
     });
   });
   socket.on('disconnect', function () {
+    
     if (addedUser) {
       delete usernames[socket.username];
       --numUsers;
@@ -51,5 +158,8 @@ io.on('connection', function (socket) {
         numUsers: numUsers
       });
     }
+  
   });
 });
+
+ 
